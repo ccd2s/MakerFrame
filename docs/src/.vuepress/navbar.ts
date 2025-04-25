921812de252eb4519617990c3d71337de682aa03
德二吹风机 <tasaedem@outlook.com>
2025-04-26 05:56:59 +0800

    优化导航栏

--- docs/src/.vuepress/navbar.ts
+++ docs/src/.vuepress/navbar.ts
@@ -5,5 +5,24 @@ export default navbar([
   "/start/",
   "/info/",
   "/projects/",
-  "/other/",
+  {
+    text: "其它",
+    icon: "ellipsis",
+    prefix: "/other/",
+    children: [
+      {
+        text: "文档",
+        children: [
+          "about.md",
+          "contribution.md",
+        ],
+      },
+      {
+        text: "引擎",
+        children: [
+          "engineUpdatelog.md"
+        ],
+      }
+    ],
+  }
 ]);
