82d0ded55380486fc37c364b73315d088864623e
德二吹风机 <tasaedem@outlook.com>
2025-05-08 21:50:09 +0800

    添加测试用 404 公益页面

--- docs/src/.vuepress/client.ts
+++ docs/src/.vuepress/client.ts
@@ -1,7 +1,16 @@
 import qqGroupLink from "./components/qqGroupLink.js";
 import { defineClientConfig } from "vuepress/client";
+import { NotFound } from "vuepress-theme-hope/client";
+import { h } from "vue";
+import NotFoundPublic from "./components/notFoundPublic.js";
+
 export default defineClientConfig({
   enhance: ({ app }) => {
     app.component("qqGroupLink", qqGroupLink);
   },
+  /* 测试用
+  layouts: {
+    NotFound: () => h(NotFound, {}, () => h(NotFoundPublic)),
+  },
+  */
 });
---
+++ docs/src/.vuepress/components/notFoundPublic.ts
@@ -0,0 +1,48 @@
+import type { VNode } from "vue";
+import { defineComponent, h } from "vue";
+import { useRouter } from "vuepress/client";
+import { useData } from "vuepress-theme-hope/client";
+
+import "../styles/notFoundPublic404.scss";
+import { Public404 } from "./notFoundPublicScript.js";
+
+export default defineComponent({
+    name: "NotFoundPublic",
+
+    setup() {
+        const { routeLocale, themeLocale } = useData();
+        const router = useRouter();
+
+        Public404("NotFoundPublic404");
+        return (): VNode[] => [
+            h("div", { class: "actions" }, [
+                h(
+                    "button",
+                    {
+                        type: "button",
+                        class: "action-button",
+                        onClick: () => {
+                            window.history.go(-1);
+                        },
+                    },
+                    themeLocale.value.routeLocales.back,
+                ),
+                h(
+                    "button",
+                    {
+                        type: "button",
+                        class: "action-button",
+                        onClick: () => {
+                            void router.push(themeLocale.value.home ?? routeLocale.value);
+                        },
+                    },
+                    themeLocale.value.routeLocales.home,
+                ),
+            ]),
+            h("div", {
+                id: "NotFoundPublic404",
+                class: "NotFoundPublic404"
+            })
+        ];
+    },
+});
